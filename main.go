04fea96dea4dd74e3e279ce649527460f89e5b46
Cmolloy36 <cdmolloy36@gmail.com>
2025-04-19 00:20:55 -0400

    handlers and stufd

--- main.go
+++ main.go
@@ -5,12 +5,23 @@ import (
 )
 
 func main() {
-	newServeMux := http.ServeMux{}
+	newServeMux := &http.ServeMux{}
 
 	newHttpServer := http.Server{
 		Addr:    ":8080",
-		Handler: &newServeMux,
+		Handler: newServeMux,
 	}
 
+	newServeMux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+
+	newServeMux.HandleFunc("/", handler)
+
 	newHttpServer.ListenAndServe()
+
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
 }
